3b9dd7f9c3231f16a040f67770561c610edeeb23
khudaykulovamir <khudaykulovamir@gmail.com>
2021-03-18 20:51:25 +0500

    Final

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -6,7 +6,8 @@
 //
 
 import UIKit
-
+import AlamofireImage
+import Parse
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var commentField: UITextField!
@@ -15,6 +16,29 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        let post  = PFObject(className: "Posts")
+        post["caption"] = commentField.text!
+        post["athor"] = PFUser.current()!
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        post["image"] = file
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved")
+            } else{
+                print("error")
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    
     @IBAction func onCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
         picker.delegate = self
@@ -25,14 +49,17 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
             picker.sourceType = .photoLibrary
         }
         present(picker, animated: true, completion: nil)
+        print("working")
     }
-    @IBAction func onSubmit(_ sender: Any) {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
         
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        imageView.image = scaledImage
+        dismiss(animated: true, completion: nil)
     }
-    
-    
-    
-    
     
     /*
     // MARK: - Navigation
